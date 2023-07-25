92f42b311ec21d38216c5030f481674ec283b333
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2023-07-25 17:00:14 +0300

    simple quiz app started

---
+++ quiz/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import quizzes from "../data.json";
+  import * as images from "../images";
+</script>
+
+<div
+  class="quizzes-container w-[700px] m-auto flex justify-center h-screen items-center"
+>
+  {#each quizzes as quiz (quiz.id)}
+    <div
+      class="quiz-card shadow w-[250px] rounded overflow-hidden cursor-pointer m-2"
+    >
+      <img src={images[quiz.imageUrl]} alt="" class="h-44" />
+      <div class="flex items-center justify-between py-3 px-2">
+        <h1 class="font-bold">{quiz.name}</h1>
+        <p>{quiz.questions.length} questions</p>
+      </div>
+    </div>
+  {/each}
+</div>
